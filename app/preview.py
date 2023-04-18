d43e523fc4d123be61ce2bd6dba89375b30d55c7
Louis Manestar <louis.manestar@gmail.com>
2023-04-18 18:41:22 +0100

    Added preview functions

---
+++ app/preview.py
@@ -0,0 +1,182 @@
+import re
+from typing import Dict, List, TypedDict
+
+import sympy
+from latex2sympy2 import latex2sympy
+from sympy.parsing import parse_expr
+from sympy.printing.latex import LatexPrinter
+from typing_extensions import NotRequired
+
+
+class Symbol(TypedDict):
+    latex: str
+    aliases: List[str]
+
+
+SymbolDict = Dict[str, Symbol]
+symbol_latex_re = re.compile(
+    r"(?P<start>\\\(|\$\$|\$)(?P<latex>.*?)(?P<end>\\\)|\$\$|\$)"
+)
+
+
+class Params(TypedDict):
+    is_latex: bool
+    simplify: NotRequired[bool]
+    symbols: NotRequired[SymbolDict]
+
+
+class Preview(TypedDict):
+    latex: str
+    sympy: str
+
+
+class Result(TypedDict):
+    preview: Preview
+
+
+def sympy_symbols(symbols: SymbolDict) -> Dict[str, sympy.Symbol]:
+    """Create a mapping of local variables for parsing sympy expressions.
+
+    Args:
+        symbols (SymbolDict): A dictionary of sympy symbol strings to LaTeX
+        symbol strings.
+
+    Note:
+        Only the sympy string is used in this function.
+
+    Returns:
+        Dict[str, sympy.Symbol]: A dictionary of sympy symbol strings to sympy
+        Symbol objects.
+    """
+    return {k: sympy.Symbol(k) for k in symbols}
+
+
+def extract_latex(symbol: str) -> str:
+    """Returns the latex portion of a symbol string.
+
+    Note:
+        Only the first matched expression is returned.
+
+    Args:
+        symbol (str): The string to extract latex from.
+
+    Returns:
+        str: The latex string.
+    """
+    if (match := symbol_latex_re.search(symbol)) is None:
+        return symbol
+
+    return match.group("latex")
+
+
+def latex_symbols(symbols: SymbolDict) -> Dict[sympy.Symbol, str]:
+    """Create a mapping between custom Symbol objects and LaTeX symbol strings.
+    Used when parsing a sympy Expression to a LaTeX string.
+
+    Args:
+        symbols (SymbolDict): A dictionary of sympy symbol strings to LaTeX
+        symbol strings.
+
+    Returns:
+        Dict[sympy.Symbol, str]: A dictionary of sympy Symbol objects to LaTeX
+        strings.
+    """
+    return {
+        sympy.Symbol(k): extract_latex(v["latex"])
+        for (k, v) in symbols.items()
+    }
+
+
+def parse_latex(response: str, symbols: SymbolDict) -> str:
+    """Parse a LaTeX string to a sympy string while preserving custom symbols.
+
+    Args:
+        response (str): The LaTeX expression to parse.
+        symbols (SymbolDict): A mapping of sympy symbol strings and LaTeX
+        symbol strings.
+
+    Raises:
+        ValueError: If the LaTeX string or symbol couldn't be parsed.
+
+    Returns:
+        str: The expression in sympy syntax.
+    """
+    substitutions = {}
+
+    for sympy_symbol_str in symbols:
+        symbol_str = symbols[sympy_symbol_str]["latex"]
+        latex_symbol_str = extract_latex(symbol_str)
+
+        try:
+            latex_symbol = latex2sympy(latex_symbol_str)
+        except Exception:
+            raise ValueError(
+                f"Couldn't parse latex symbol {latex_symbol_str} "
+                f"to sympy symbol."
+            )
+
+        substitutions[latex_symbol] = sympy.Symbol(sympy_symbol_str)
+
+    try:
+        expression = latex2sympy(response, substitutions)
+
+        if isinstance(expression, list):
+            expression = expression.pop()
+
+        return str(expression.xreplace(substitutions))  # type: ignore
+
+    except Exception as e:
+        raise ValueError(str(e))
+
+
+def preview_function(response: str, params: Params) -> Result:
+    """
+    Function used to preview a student response.
+    ---
+    The handler function passes three arguments to preview_function():
+
+    - `response` which are the answers provided by the student.
+    - `params` which are any extra parameters that may be useful,
+        e.g., error tolerances.
+
+    The output of this function is what is returned as the API response
+    and therefore must be JSON-encodable. It must also conform to the
+    response schema.
+
+    Any standard python library may be used, as well as any package
+    available on pip (provided it is added to requirements.txt).
+
+    The way you wish to structure you code (all in this function, or
+    split into many) is entirely up to you.
+    """
+    symbols: SymbolDict = params.get("symbols", {})
+
+    if not response:
+        return Result(preview=Preview(latex="", sympy=""))
+
+    try:
+        if params.get("is_latex", False):
+            response = parse_latex(response, symbols)
+
+        equation = parse_expr(
+            response,
+            evaluate=False,
+            local_dict=sympy_symbols(symbols),
+            transformations="all",
+        )
+
+        if params.get("simplify", False):
+            equation = sympy.simplify(equation)
+
+        latex_out = LatexPrinter(
+            {"symbol_names": latex_symbols(symbols)}
+        ).doprint(equation)
+
+        sympy_out = str(equation)
+
+    except SyntaxError as e:
+        raise ValueError("Failed to parse Sympy expression") from e
+    except ValueError as e:
+        raise ValueError("Failed to parse LaTeX expression") from e
+
+    return Result(preview=Preview(latex=latex_out, sympy=sympy_out))
---
+++ app/preview_tests.py
@@ -0,0 +1,255 @@
+import unittest
+
+try:
+    from .preview import Params, extract_latex, preview_function
+except ImportError:
+    from preview import Params, extract_latex, preview_function
+
+
+class TestPreviewFunction(unittest.TestCase):
+    """
+    TestCase Class used to test the algorithm.
+    ---
+    Tests are used here to check that the algorithm written
+    is working as it should.
+
+    It's best practise to write these tests first to get a
+    kind of 'specification' for how your algorithm should
+    work, and you should run these tests before committing
+    your code to AWS.
+
+    Read the docs on how to use unittest here:
+    https://docs.python.org/3/library/unittest.html
+
+    Use preview_function() to check your algorithm works
+    as it should.
+    """
+
+    def test_empty_latex_expression(self):
+        response = ""
+        params = Params(is_latex=True)
+        result = preview_function(response, params)
+        self.assertIn("preview", result)
+
+        preview = result["preview"]
+        self.assertEqual(preview["latex"], "")
+
+    def test_empty_sympy_expression(self):
+        response = ""
+        params = Params(is_latex=False)
+        result = preview_function(response, params)
+        self.assertIn("preview", result)
+        self.assertNotIn("error", result)
+
+        preview = result["preview"]
+        self.assertEqual(preview["sympy"], "")
+
+    def test_latex_and_sympy_are_returned(self):
+        response = "x+1"
+        params = Params(is_latex=True)
+        result = preview_function(response, params)
+        self.assertIn("preview", result)
+        self.assertNotIn("error", result)
+
+        preview = result["preview"]
+
+        self.assertIn("latex", preview)
+        self.assertIn("sympy", preview)
+
+        response = "x+1"
+        params = Params(is_latex=False)
+        result = preview_function(response, params)
+        self.assertIn("preview", result)
+        self.assertNotIn("error", result)
+
+        preview = result["preview"]
+
+        self.assertIn("latex", preview)
+        self.assertIn("sympy", preview)
+
+    def test_doesnt_simplify_latex_by_default(self):
+        response = "\\frac{x + x^2 + x}{x}"
+        params = Params(is_latex=True)
+        result = preview_function(response, params)
+        preview = result["preview"]
+
+        self.assertEqual(preview.get("sympy"), "(x**2 + x + x)/x")
+
+    def test_doesnt_simplify_sympy_by_default(self):
+        response = "(x + x**2 + x)/x"
+        params = Params(is_latex=False)
+        result = preview_function(response, params)
+        self.assertNotIn("error", result)
+        preview = result["preview"]
+
+        self.assertEqual(preview.get("latex"), "\\frac{x^{2} + x + x}{x}")
+
+    def test_simplifies_latex_on_param(self):
+        response = "\\frac{x + x^2 + x}{x}"
+        params = Params(is_latex=True, simplify=True)
+        result = preview_function(response, params)
+        self.assertNotIn("error", result)
+        preview = result["preview"]
+
+        self.assertEqual(preview.get("sympy"), "x + 2")
+
+    def test_simplifies_sympy_on_param(self):
+        response = "(x + x**2 + x)/x"
+        params = Params(is_latex=False, simplify=True)
+        result = preview_function(response, params)
+        self.assertNotIn("error", result)
+        preview = result["preview"]
+
+        self.assertEqual(preview.get("latex"), "x + 2")
+
+    def test_sympy_handles_implicit_multiplication(self):
+        response = "sin(x) + cos(2x) - 3x**2"
+        params = Params(is_latex=False)
+        result = preview_function(response, params)
+        self.assertNotIn("error", result)
+
+        preview = result["preview"]
+
+        self.assertEqual(
+            preview.get("latex"),
+            "- 3 x^{2} + \\sin{\\left(x \\right)} "
+            "+ \\cos{\\left(2 x \\right)}",
+        )
+
+    def test_latex_with_equality_symbol(self):
+        response = "\\frac{x + x^2 + x}{x} = y"
+
+        params = Params(is_latex=True, simplify=False)
+        result = preview_function(response, params)
+        self.assertNotIn("error", result)
+
+        preview = result["preview"]
+
+        self.assertEqual(preview.get("sympy"), "Eq((x**2 + x + x)/x, y)")
+
+    def test_sympy_with_equality_symbol(self):
+        response = "Eq((x + x**2 + x)/x, 1)"
+        params = Params(is_latex=False, simplify=False)
+        result = preview_function(response, params)
+        self.assertNotIn("error", result)
+
+        preview = result["preview"]
+
+        self.assertEqual(preview.get("latex"), "\\frac{x^{2} + x + x}{x} = 1")
+
+    def test_latex_conversion_preserves_default_symbols(self):
+        response = "\\mu + x + 1"
+        params = Params(is_latex=True, simplify=False)
+        result = preview_function(response, params)
+        self.assertNotIn("error", result)
+
+        preview = result["preview"]
+
+        self.assertEqual(preview.get("sympy"), "mu + x + 1")
+
+    def test_sympy_conversion_preserves_default_symbols(self):
+        response = "mu + x + 1"
+        params = Params(is_latex=False, simplify=False)
+        result = preview_function(response, params)
+        self.assertNotIn("error", result)
+
+        preview = result["preview"]
+
+        self.assertEqual(preview.get("latex"), "\\mu + x + 1")
+
+    def test_latex_conversion_preserves_optional_symbols(self):
+        response = "m_{ \\text{table} } + \\text{hello}_\\text{world} - x + 1"
+        params = Params(
+            is_latex=True,
+            simplify=False,
+            symbols={
+                "m_table": {
+                    "latex": r"hello \( m_{\text{table}} \) world",
+                    "aliases": [],
+                },
+                "test": {
+                    "latex": r"hello $ \text{hello}_\text{world} $ world.",
+                    "aliases": [],
+                },
+            },
+        )
+        result = preview_function(response, params)
+        self.assertNotIn("error", result)
+
+        preview = result["preview"]
+
+        self.assertEqual(preview.get("sympy"), "m_table + test - x + 1")
+
+    def test_sympy_conversion_preserves_optional_symbols(self):
+        response = "m_table + test + x + 1"
+        params = Params(
+            is_latex=False,
+            simplify=False,
+            symbols={
+                "m_table": {"latex": "m_{\\text{table}}", "aliases": []},
+                "test": {
+                    "latex": "\\text{hello}_\\text{world}",
+                    "aliases": [],
+                },
+            },
+        )
+        result = preview_function(response, params)
+        self.assertNotIn("error", result)
+
+        preview = result["preview"]
+
+        self.assertEqual(
+            preview.get("latex"),
+            "m_{\\text{table}} + \\text{hello}_\\text{world} + x + 1",
+        )
+
+    def test_invalid_latex_returns_error(self):
+        response = "\frac{ m_{ \\text{table} } + x + 1 }{x"
+        params = Params(
+            is_latex=True,
+            simplify=False,
+            symbols={"m_table": {"latex": "m_{\\text{table}}", "aliases": []}},
+        )
+
+        with self.assertRaises(ValueError):
+            preview_function(response, params)
+
+    def test_invalid_sympy_returns_error(self):
+        response = "x + x***2 - 3 / x 4"
+        params = Params(simplify=False, is_latex=False)
+
+        with self.assertRaises(ValueError):
+            preview_function(response, params)
+
+    def test_extract_latex_in_delimiters(self):
+        parentheses = r"\( x + 1 \)"
+        dollars = r"$ x ** 2 + 1 $"
+        double_dollars = r"$$ \sin x + \tan x $$"
+
+        self.assertEqual(extract_latex(parentheses), " x + 1 ")
+        self.assertEqual(extract_latex(dollars), " x ** 2 + 1 ")
+        self.assertEqual(extract_latex(double_dollars), r" \sin x + \tan x ")
+
+    def test_extract_latex_in_delimiters_and_text(self):
+        parentheses = r"hello \( x + 1 \) world."
+        dollars = r"hello $ x ** 2 + 1 $ world."
+
+        self.assertEqual(extract_latex(parentheses), " x + 1 ")
+        self.assertEqual(extract_latex(dollars), " x ** 2 + 1 ")
+
+    def test_extract_latex_no_delimiters(self):
+        test = r"'\sin x + \left ( \text{hello world} \right ) + \cos x"
+        self.assertEqual(extract_latex(test), test)
+
+    def test_extract_latex_multiple_expressions(self):
+        parentheses = r"hello \( x + 1 \) world. \( \sin x + \cos x \) yes."
+        dollars = r"hello $ x ** 2 + 1 $ world. \( \sigma \times \alpha \) no."
+        mixture = r"hello $ x ** 2 - 1 $ world. $ \sigma \times \alpha $ !."
+
+        self.assertEqual(extract_latex(parentheses), " x + 1 ")
+        self.assertEqual(extract_latex(dollars), " x ** 2 + 1 ")
+        self.assertEqual(extract_latex(mixture), " x ** 2 - 1 ")
+
+
+if __name__ == "__main__":
+    unittest.main()
